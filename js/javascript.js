39593684560122175caa8a17e2d2a3ba6087cc5e
Eudesmagno Manuel João Miguel <87705679+Eudesmagnomiguel@users.noreply.github.com>
2021-10-11 16:01:47 +0100

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,21 @@
+
+$(".btn_close").click(function(){
+
+$(".menu-mobile").animate({
+  right:'-100%'
+});
+
+});
+
+
+
+$(".toolbar").click(function(){
+
+  $(".menu-mobile").animate({
+    right:'0%'
+  });
+  
+  });
+  
+  
+  
